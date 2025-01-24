9e75cec92cb539c339d95757e725d87c43c435ba
Darwin L <lemus@unc.edu>
2025-01-23 20:06:09 -0500

    Got rid of the animation that shit doesnt work

--- script.js
+++ script.js
@@ -40,17 +40,6 @@ window.addEventListener('scroll', () => {
     });
 });
 
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('fade-in');
-        }
-    });
-});
-
-document.querySelectorAll('section').forEach(section => {
-    observer.observe(section);
-});
 
 // Optional: Add 'active' class styling in CSS
 // .active {
